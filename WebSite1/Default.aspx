699a284e920436fb4141f1873af75b681e1af767
sanjeev <sanjk0604@gmail.com>
2018-04-11 17:31:59 -0700

    Add project files.

---
+++ WebSite1/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+	<div class="jumbotron">
+		<h1>ASP.NET</h1>
+		<p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
+		<p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+	</div>
+
+	<div class="row">
+		<div class="col-md-4">
+			<h2 runat="server" id="output">Get IP Address: </h2>
+			<asp:Button ID="Button1" runat="server" Text="Get IP Address!" OnClick="Button1_Click" />
+		</div>
+		<div class="col-md-4">
+			<h2 runat="server" id="output2" visible="false"> </h2>
+			<asp:Button ID="Button2" runat="server" Text="Write to Cookie!" OnClick="Button2_Click" visible="false" />
+		</div>
+		<div class="col-md-4">
+		</div>
+	</div>
+</asp:Content>
---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e){}
+
+	protected void Button1_Click(object sender, EventArgs e)
+	{
+		// Get and display IP Address
+		string ipAddress = GetLocalIPAddress();
+
+		output.InnerHtml = "";
+		output.InnerHtml += $"Ip address is: {ipAddress}";
+
+		// Get City & State info
+		CityStateCountByIp(ipAddress);
+
+		// Display "Write Cookie Button"
+		Button2.Visible = true;
+
+	}
+
+	public string GetLocalIPAddress()
+	{
+		var host = Dns.GetHostEntry(Dns.GetHostName());
+
+		string externalip = new WebClient().DownloadString("http://icanhazip.com");
+		return externalip;
+	}
+
+	public void CityStateCountByIp(string ip)
+	{
+		var url = "http://freegeoip.net/json/" + ip;
+		var request = System.Net.WebRequest.Create(url);
+
+		using (WebResponse wrs = request.GetResponse())
+		using (Stream stream = wrs.GetResponseStream())
+		using (StreamReader reader = new StreamReader(stream))
+		{
+			string json = reader.ReadToEnd();
+			var obj = JObject.Parse(json);
+
+			IP.State = (string)obj["region_name"];
+			IP.City = (string)obj["city"];
+			IP.IPAddress = ip;
+		};
+
+	}
+
+	protected void Button2_Click(object sender, EventArgs e)
+	{
+		// Write Cookie
+		Response.Cookies["UserSettings"]["IP"] = IP.IPAddress;
+		Response.Cookies["UserSettings"]["City"] = IP.City;
+		Response.Cookies["UserSettings"]["State"] = IP.State;
+
+		Response.Cookies["UserSettings"].Expires = DateTime.Now.AddDays(1d);
+	}
+}
+
+public static class IP
+{
+	public static string City { get; set; }
+	public static string State { get; set; }
+	public static string IPAddress { get; set; }
+}
